011b6d965f1af71373ed65cdb7c1c8d4115c7e3b
jwozniak <jakubwozniak0611@gmail.com>
2023-11-29 11:23:47 +0100

    Fixed Lab05

--- Lab05/main.py
+++ Lab05/main.py
@@ -1,4 +1,7 @@
+from itertools import permutations
+
 import networkx as nx
+import numpy as np
 
 
 def triangle(e1, e2, e3):
@@ -16,6 +19,25 @@ def traingle_inequality(G):
     return True
 
 
+def min_perfect_matching(G):
+    # maximal_matching = nx.maximal_matching(G)
+    # min_weight_matching = None
+    # min_weight = np.inf
+    #
+    # for permuted_matching in permutations(maximal_matching):
+    #     weight = sum(G[edge[0]][edge[1]]['weight'] for edge in permuted_matching)
+    #
+    #     if weight < min_weight:
+    #         min_weight = weight
+    #         min_weight_matching = permuted_matching
+
+    is_perfect = nx.is_perfect_matching(G, nx.min_weight_matching(G))
+    if not is_perfect:
+        raise Exception('Cannot find a perfect matching')
+
+    return nx.min_weight_matching(G)
+
+
 def christofides(G):
     if not traingle_inequality(G):
         return Exception("Graph does not satisfy triangle inequality")
@@ -24,15 +46,8 @@ def christofides(G):
     T = nx.minimum_spanning_tree(G)
 
     # 2
-    O = [v for v, d in T.degree() if d % 2 == 1]
-
-    M = nx.Graph()
-    M.add_nodes_from(O)
-    for u in O:
-        for v in O:
-            if u < v and G.has_edge(u, v):
-                M.add_edge(u, v, weight=G[u][v]['weight'])
-    M = nx.max_weight_matching(M, maxcardinality=True)
+    O = G.subgraph([node for node, degree in T.degree() if degree % 2 == 1])
+    M = min_perfect_matching(O)
 
     # 3
     H = nx.MultiGraph(T)
@@ -42,7 +57,15 @@ def christofides(G):
     eulerian_circuit = nx.eulerian_circuit(H)
 
     # 5
-    hamiltonian_circuit = [u for u, v in eulerian_circuit]
+
+    hamiltonian_circuit = []
+    for u, v in eulerian_circuit:
+        if u not in hamiltonian_circuit:
+            hamiltonian_circuit.append(u)
+        if v not in hamiltonian_circuit:
+            hamiltonian_circuit.append(v)
+
+    # hamiltonian_circuit.append(hamiltonian_circuit[0])
 
     weight = 0
     for i in range(len(hamiltonian_circuit) - 1):
@@ -59,5 +82,4 @@ if __name__ == "__main__":
     G.add_nodes_from([0, 1, 2, 3, 4])
     G.add_weighted_edges_from(
         [(0, 1, 1), (0, 2, 1), (0, 3, 2), (0, 4, 3), (1, 2, 2), (1, 3, 2), (1, 4, 3), (2, 3, 3), (2, 4, 2), (3, 4, 2)])
-
     print(christofides(G))
